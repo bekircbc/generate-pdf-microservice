8150effc0dc1869fbfd07c5fbc53a3dd6c16c05f
Bekir Cebeci <62771823+bekircbc@users.noreply.github.com>
2024-05-05 13:00:19 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -44,7 +44,7 @@ createConnection()
     });
 
     app.get('/', (req, res) => {
-      res.sendFile(path.join(__dirname, '../index.html'));
+      res.sendFile(path.join(__dirname, './index.html'));
     });
 
     app.listen(32465, () => {
